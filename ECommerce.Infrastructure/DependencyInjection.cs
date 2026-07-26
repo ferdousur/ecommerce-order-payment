b1df6e8edbd41715c99ffe490762902d31dd3984
Ferdousur Rahman <ferdousur.info@gmail.com>
2026-07-26 16:13:38 +0600

    Bug Fix: AddScoped IUserservice and Userservice

--- ECommerce.Infrastructure/DependencyInjection.cs
+++ ECommerce.Infrastructure/DependencyInjection.cs
@@ -31,6 +31,8 @@ public static class DependencyInjections
         services.AddScoped<IPaymentWebhookHandler, StripeWebhookHandler>();
         services.AddHttpClient<BkashPaymentProcessor>();
         services.AddScoped<IPaymentProcessor, BkashPaymentProcessor>();
+        services.AddScoped<IUserService, UserService>();
+
         return services;
     }
 }
